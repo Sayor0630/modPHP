9c8b9274f50c58eee289102dd0375e13fa3a9ade
aronno000 <prosad.sen.000@gmail.com>
2023-10-18 16:26:35 +0600

    update

--- auth/login.php
+++ auth/login.php
@@ -13,15 +13,6 @@
 </head>
 
 <body>
-  <nav>
-    <div class="logo">
-      <img src=".././img/logo/logo.png" alt="Logo">
-    </div>
-    <div class="signin">
-      <a href=".././index.html">Home</a></li>
-    </div>
-  </nav>
-
   <div class="login-card">
     <img src="https://i.pinimg.com/originals/0a/5f/ea/0a5feae400fc816c4ca2aca8bd67a168.jpg" />
     <h2>Sign In</h2>
@@ -49,36 +40,10 @@
       <button class="btn" id="signup-btn" type="submit">
         <p>Login</p>
       </button>
-      <h5>Do not have an account? <a href="./signup.html">Sign Up now</a> </h5>
+      <h5>Do not have an account? <a href="./signup.php">Sign Up now</a> </h5>
     </form>
   </div>
 
-  <footer>
-    <div class="top-section">
-      <div class="left-subsection">
-        <img src="./img/logo/logo.png" alt="Logo">
-      </div>
-      <div class="middle-subsection">
-        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-telephone-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd"
-              d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-          </svg> +880 18**-******</p>
-
-        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-envelope-fill" viewBox="0 0 16 16">
-            <path
-              d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-          </svg>afsana_apu@gmail.com</p>
-      </div>
-      <div class="right-subsection">
-        <h4>Developed by <a href="https://www.facebook.com/oyenja/">Oyenja</a></h4>
-      </div>
-    </div>
-    <div class="bottom-section">
-      <p>&copy; 2023 All rights reserved.</p>
-    </div>
-  </footer>
 
   <script type="text/javascript" src="./main.js"></script>
   <script type="text/javascript" src="./pass-gen.js"></script>
--- auth/signup.php
+++ auth/signup.php
@@ -15,14 +15,6 @@
 </head>
 
 <body>
-  <nav>
-    <div class="logo">
-      <img src=".././img/logo/logo.png" alt="Logo">
-    </div>
-    <div class="signin">
-      <a href=".././index.html">Home</a></li>
-    </div>
-  </nav>
 
 
 
@@ -46,11 +38,8 @@
         </div>
         <div class="mobile-number">
         <div class="phone-input">
-          <select name="countryCode" id="countryCode" class="control countryCode">
+          <select name="countryCode" id="countryCode" class="control countryCode" disabled>
             <option value="+880">+880 (BD)</option>
-            <option value="+1">+1 (US)</option>
-            <option value="+44">+44 (UK)</option>
-            <!-- Add more country codes here -->
           </select>
           <input autocomplete="off" spellcheck="false" class="control mobileNumber" type="tel" id="mobile_number"
             name="mobile_number" placeholder="Mobile Number" />
@@ -209,39 +198,13 @@
 <button class="btn" id="signup-btn" type="submit">
   <p>JOIN NOW</p>
 </button>
-        <h5>Already have an account? <a href="./login.html">Login now</a> </h5>
+        <h5>Already have an account? <a href="./login.php">Login now</a> </h5>
       </form>
     </div>
   </div>
 
   <!-- ==================================================================================== -->
 
-  <footer>
-    <div class="top-section">
-      <div class="left-subsection">
-        <img src="./img/logo/logo.png" alt="Logo">
-      </div>
-      <div class="middle-subsection">
-        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-telephone-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd"
-              d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-          </svg> +880 18**-******</p>
-
-        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-            class="bi bi-envelope-fill" viewBox="0 0 16 16">
-            <path
-              d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
-          </svg>afsana_apu@gmail.com</p>
-      </div>
-      <div class="right-subsection">
-        <h4>Developed by <a href="https://www.facebook.com/oyenja/">Oyenja</a></h4>
-      </div>
-    </div>
-    <div class="bottom-section">
-      <p>&copy; 2023 All rights reserved.</p>
-    </div>
-  </footer>
   <script type="text/javascript" src="./main.js"></script>
   <script type="text/javascript" src=".././js/vanilla-tilt.js"></script>
   <script>
